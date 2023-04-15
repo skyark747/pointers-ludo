420ae82d72e96d3276fe64815df24ff167ab1036
skyark747 <bscs22064@itu.edu.pk>
2023-04-15 13:16:57 -0700

    add menu

--- pointers ludo/Menu.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define Max_main_menu 3
-class Menu
-{
-public:
-private:
-	
-};
-
--- pointers ludo/pointers ludo.cpp
+++ pointers ludo/pointers ludo.cpp
@@ -1,16 +1,18 @@
 // pointers ludo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
-#include <windows.h>
 #include "Board.h"
 #include "Ludo.h"
 #include "Utility.h"
-#include "SFML/Graphics.hpp"
+#include "Menu.h"
+#include <SFML/Graphics.hpp>
 using namespace std;
+//using namespace sf;
 
 int main()
 {
-    Ludo L;
-    L.Play();
-    return 0;
+ 	Ludo L;
+	L.Play();
+	return 0;
 }
+
